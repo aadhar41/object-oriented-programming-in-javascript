f1c5dbd9a9665657c40673875a6a26db7821b075
Aadhar gaur <aadhar41@gmail.com>
2023-11-14 10:46:14 +0530

    008-3-Factories

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+// Factory Function
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function name(radius) {
+            console.log('Draw');
+        }
+    };
+}
 
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1,
-    },
-    draw: function name(params) {
-        console.log('Draw');
-    }
-};
-
+const circle = createCircle(5);
 circle.draw();
-
