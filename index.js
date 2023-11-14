3182da18120a7267842c21b9fdf9b79e7c400991
Aadhar gaur <aadhar41@gmail.com>
2023-11-14 14:46:40 +0530

    017 Getters and Setters

--- index.js
+++ index.js
@@ -2,13 +2,24 @@
 function Circle(radius) {
     this.radius = radius;
     let defaultLocation = {x : 0, y : 0};
-    let computeOptimumLocation = function (param) {
-
-    };
+    this.getDefaultLocation = function (param) {
+        return defaultLocation;
+    },
     this.draw = function (params) {
         computeOptimumLocation(0, 1);
         console.log('draw');
-    }
+    },
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function () {
+            return defaultLocation;
+        },
+        set: function (value) {
+            if(!value.x || !value.y)
+                throw new Error('Invalid Location.');
+            defaultLocation = value;
+        }
+    });
 }
 
 const circle = new Circle(10);
+circle.defaultLocation = 0;
