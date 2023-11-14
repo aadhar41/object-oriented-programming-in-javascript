757e45b9313b16be9058d62068be627321d5a367
Aadhar gaur <aadhar41@gmail.com>
2023-11-14 14:21:37 +0530

    016 11- Private Properties and Methods

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 // Constructor Function
 function Circle(radius) {
     this.radius = radius;
+    let defaultLocation = {x : 0, y : 0};
+    let computeOptimumLocation = function (param) {
+
+    };
     this.draw = function (params) {
+        computeOptimumLocation(0, 1);
         console.log('draw');
     }
 }
 
 const circle = new Circle(10);
-
-for(let key in circle) {
-    if(typeof circle[key] !== 'function')
-        console.log(key, circle[key]);
-}
-
-const keys = Object.keys(circle);
-console.log(keys);
-
-if('radius' in circle)
-    console.log('Circle has a radius.');
