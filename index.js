870bac9196def2d407681c984388b136ba9badae
Aadhar gaur <aadhar41@gmail.com>
2023-11-14 10:38:11 +0530

    007 2- Object Literals

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+    draw: function name(params) {
+        console.log('Draw');
+    }
+};
+
+circle.draw();
+
